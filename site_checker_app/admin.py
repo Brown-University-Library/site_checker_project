77dece8e22bc441417de421e2443e44844abc4a5
birkin <birkin.diana@gmail.com>
2018-01-30 17:11:23 -0500

    handles long urls in admin list.

--- site_checker_app/admin.py
+++ site_checker_app/admin.py
@@ -4,10 +4,20 @@ from django.contrib import admin
 from site_checker_app.models import CheckSite
 
 
-class CheckSiteAdmin(admin.ModelAdmin):
+class CheckSiteAdmin( admin.ModelAdmin ):
     save_on_top = True
-    list_display = [ 'name', 'url', 'text_expected', 'recent_checked_time', 'recent_checked_result', 'next_check_time' ]
+    list_display = [ 'name', 'partial_url', 'text_expected', 'recent_checked_time', 'recent_checked_result', 'next_check_time' ]
     ordering = [ 'name' ]
+
+    def partial_url(self, obj):
+        """ Specifies appearance of url in list_display. """
+        p_url = ''
+        if len(obj.url) <= 50:
+            p_url = obj.url
+        else:
+            p_url = '%s...' % obj.url[0:47]
+        return p_url
+    partial_url.short_description = 'url'
 
 
 admin.site.register( CheckSite, CheckSiteAdmin )
