97eb60baad6e524743ccbc264bd5f6b2d9385caa
birkin <birkin.diana@gmail.com>
2019-05-24 10:56:44 -0400

    adds required settings for email-admin-on-error.

--- config/settings.py
+++ config/settings.py
@@ -124,6 +124,7 @@ STATIC_ROOT = os.environ['SITECHKR__STATIC_ROOT']  # needed for collectstatic co
 
 
 # Email
+SERVER_EMAIL = 'site_checker@library.brown.edu'
 EMAIL_HOST = os.environ['SITECHKR__EMAIL_HOST']
 EMAIL_PORT = int( os.environ['SITECHKR__EMAIL_PORT'] )
 
@@ -164,6 +165,11 @@ LOGGING = {
             'class':'logging.StreamHandler',
             'formatter': 'standard'
         },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+            'include_html': True,
+        }
     },
     'loggers': {
         'site_checker_app': {
@@ -171,5 +177,10 @@ LOGGING = {
             'level': os.environ.get(u'SITECHKR__LOG_LEVEL'),
             'propagate': False
         },
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': False,
+        }
     }
 }
