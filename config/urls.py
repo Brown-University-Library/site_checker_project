5bca70d8f8157d6940f78fecf88218fc55f44987
birkin <birkin.diana@gmail.com>
2019-05-24 10:11:33 -0400

    adds error-check url.

--- config/urls.py
+++ config/urls.py
@@ -13,10 +13,13 @@ urlpatterns = [
 
     ## primary app urls
     url( r'^admin/', admin.site.urls ),  # eg host/project_x/admin/
-    url( r'^info/$', views.info, name='info_url' ),
     url( r'^status/$', views.show_status, name='show_status_url' ),
-    url( r'^status_old/$', views.show_status_old, name='show_status_old_url' ),
+    # url( r'^status_old/$', views.show_status_old, name='show_status_old_url' ),
     url( r'^login/$', views.login, name='admin_login_url' ),
+
+    ## info urls
+    url( r'^error_check/$', views.info, name='error_check_url' ),
+    url( r'^info/$', views.info, name='info_url' ),
 
     ## support urls
     url( r'^bul_search/$', views.bul_search, name='bul_search_url' ),
