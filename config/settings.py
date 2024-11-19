a97e72903981bd0940f004f6e769c35c2076e0d0
Birkin James Diana <birkin.diana@gmail.com>
2024-11-19 09:03:41 -0500

    adds debug print statements.

--- config/settings.py
+++ config/settings.py
@@ -19,10 +19,14 @@ import pathlib
 
 from dotenv import find_dotenv, load_dotenv
 
+print('here00')
+
 ## load envars ------------------------------------------------------
 dotenv_path = pathlib.Path(__file__).resolve().parent.parent.parent / '.env'
 assert dotenv_path.exists(), f'file does not exist, ``{dotenv_path}``'
+print('here01')
 load_dotenv(find_dotenv(str(dotenv_path), raise_error_if_not_found=True), override=True)
+print('here02')
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
