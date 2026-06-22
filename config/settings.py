e29de0d20fe3cebc55af4b0db3967bceb7c200cb
Nicole Currens <nicole_currens@brown.edu>
2026-06-22 11:14:16 -0700

    setup

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ import pathlib
 from dotenv import find_dotenv, load_dotenv
 
 ## load envars ------------------------------------------------------
-dotenv_path = pathlib.Path(__file__).resolve().parent.parent.parent / '.env'
+dotenv_path = dotenv_path = pathlib.Path(__file__).resolve().parent.parent / '.env'
 assert dotenv_path.exists(), f'file does not exist, ``{dotenv_path}``'
 load_dotenv(find_dotenv(str(dotenv_path), raise_error_if_not_found=True), override=True)
 
